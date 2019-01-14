2bfa27ac72a725b030cd401ac09aa95940309eba
Francesco Lo Sterzo <francesco.losterzo@naturalcycles.com>
2019-01-14 21:21:12 +0100

    updates in utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,6 @@
 ## Here write useful functions to make the Notebook cleaner
+import socket
+
+def getDataPath():
+    if (socket.gethostname()=='DESKTOP-AK02NII'): return '/home/francesco/Projects/data/kaggle-titanic'
+    return ''
