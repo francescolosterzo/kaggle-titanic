b8d5debf37b84b8f8ed7e8339e9e88b50aa62efc
Francesco Lo Sterzo <francesco.losterzo@naturalcycles.com>
2019-01-17 18:34:19 +0100

    m_00 done

--- utils.py
+++ utils.py
@@ -2,5 +2,23 @@
 import socket
 
 def getDataPath():
+    """
+    returns the path to the data files based on the device you are working on
+    """
     if (socket.gethostname()=='DESKTOP-AK02NII'): return '/home/francesco/Projects/data/kaggle-titanic'
+    if (socket.gethostname()=='Francescos-MBP'): return '/Users/francesco/PersonalProjects/data/kaggle-titanic'
     return ''
+
+def replaceElementInList(oldElement, newElement, List):
+    """
+    replaces all the occurrences of oldElement in List with newElement
+    Return value: the new list
+    """
+    
+    newList = List.copy()
+    
+    for n, i in enumerate(newList):
+        if i==oldElement:
+            newList[n] = newElement
+    
+    return newList
