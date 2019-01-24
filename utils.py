1807bf633816687f50968a8e494c73765192552a
Francesco Lo Sterzo <francesco.losterzo@naturalcycles.com>
2019-01-24 18:35:38 +0100

    m00 completed, included features ranking

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 ## Here write useful functions to make the Notebook cleaner
 import socket
+import numpy as np
 
 def getDataPath():
     """
@@ -22,3 +23,28 @@ def replaceElementInList(oldElement, newElement, List):
             newList[n] = newElement
     
     return newList
+
+def getFullParameters(fit_result, x):
+    """
+    get the full parameters (coefficients and errors) out of the fit result for a LogisticRegression model.
+    The procedure is taken from here: https://stats.stackexchange.com/questions/89484/how-to-compute-the-standard-errors-of-a-logistic-regressions-coefficients
+    Return value: arrays containing coefficients and errors (intercept in the first position)
+    """
+    
+    # probability matrix
+    probs = fit_result.predict_proba(x)
+
+    # design matrix: add a column of 1 for the intercept
+    x_design = np.hstack([np.ones((x.shape[0], 1)), x])
+
+    V = np.diagflat(np.product(probs, axis=1))
+
+    # covariance matrix
+    cov = np.linalg.inv(np.dot(np.dot(x_design.T, V), x_design))
+    
+    # errors
+    errors = np.sqrt(np.diag(cov))
+
+    params = np.insert(fit_result.coef_, 0, fit_result.intercept_)
+    
+    return params, errors
